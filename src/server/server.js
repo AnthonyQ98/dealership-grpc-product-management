9977584db4e7345c7f259dd53e88816005399cb5
Anthony Quinn <anthonyquinn.developer@gmail.com>
2024-05-03 17:13:23 +0100

    Server implementation

---
+++ src/server/server.js
@@ -0,0 +1,33 @@
+// Import required modules
+const grpc = require('@grpc/grpc-js');
+const protoloader = require('@grpc/proto-loader');
+
+// Import service implementations
+const { updateCarInventory, addCar, listAllCars, getCarDetails } = require('../services/product_management');
+
+// Load protocol buffer for dealership service
+const dealershipPackageDefinition = protoloader.loadSync('protos/product_service.proto', {});
+const dealershipProto = grpc.loadPackageDefinition(dealershipPackageDefinition).stock_management;
+
+// Create gRPC server
+const server = new grpc.Server();
+
+// Define server address
+const serverAddr = '0.0.0.0:50051';
+
+// Create an object containing service implementation
+const services = {
+    Update: updateCarInventory,
+    Add: addCar,
+    List: listAllCars, 
+    Get: getCarDetails,
+};
+
+// Add service to the gRPC server
+server.addService(dealershipProto.service, services);
+
+
+// Bind the server to the specified address and start it
+server.bindAsync(serverAddr, grpc.ServerCredentials.createInsecure(), () => {
+    console.log("Product management server running at " + serverAddr);
+});
